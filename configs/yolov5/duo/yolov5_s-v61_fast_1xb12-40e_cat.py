58c20729a3b5134cdd430de85034dfdf317877ff
Junjie Wen <junjiewen@JunjiedeMacBook-Air.local>
2023-08-25 10:38:04 +0800

    just for test
    Signed-off-by: Junjie Wen <junjiewen@JunjiedeMacBook-Air.local>

--- configs/yolov5/duo/yolov5_s-v61_fast_1xb12-40e_cat.py
+++ configs/yolov5/duo/yolov5_s-v61_fast_1xb12-40e_cat.py
@@ -18,7 +18,7 @@ anchors = [
 
 max_epochs = 40
 train_batch_size_per_gpu = 12
-train_num_workers = 4
+train_num_workers = 0
 save_checkpoint_intervals = 10
 
 load_from = 'https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-v61_syncbn_fast_8xb16-300e_coco/yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth'  # noqa
@@ -47,69 +47,69 @@ train_dataloader = dict(
 #     dataset=dict(
 #         type=dataset_type))
 
-albu_train_transforms = [
-    dict(type='Blur', p=0.01),
-    dict(type='MedianBlur', p=0.01),
-    dict(type='ToGray', p=0.01),
-    dict(type='CLAHE', p=0.01)
-]
+# albu_train_transforms = [
+#     dict(type='Blur', p=0.01),
+#     dict(type='MedianBlur', p=0.01),
+#     dict(type='ToGray', p=0.01),
+#     dict(type='CLAHE', p=0.01)
+# ]
+#
+# pre_transform = [
+#     dict(type='LoadImageFromFile', backend_args=_base_.backend_args),
+#     dict(type='LoadAnnotations', with_bbox=True)
+# ]
 
-pre_transform = [
-    dict(type='LoadImageFromFile', backend_args=_base_.backend_args),
-    dict(type='LoadAnnotations', with_bbox=True)
-]
-
-train_pipeline1 = [
-    *pre_transform,
-    dict(
-        type='Mosaic',
-        img_scale=img_scale,
-        pad_val=114.0,
-        pre_transform=pre_transform),
-    dict(
-        type='YOLOv5RandomAffine',
-        max_rotate_degree=0.0,
-        max_shear_degree=0.0,
-        scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
-        # img_scale is (width, height)
-        border=(-img_scale[0] // 2, -img_scale[1] // 2),
-        border_val=(114, 114, 114)),
-    dict(
-        type='mmdet.Albu',
-        transforms=albu_train_transforms,
-        bbox_params=dict(
-            type='BboxParams',
-            format='pascal_voc',
-            label_fields=['gt_bboxes_labels', 'gt_ignore_flags']),
-        keymap={
-            'img': 'image',
-            'gt_bboxes': 'bboxes'
-        }),
-    dict(type='YOLOv5HSVRandomAug'),
-    dict(type='mmdet.RandomFlip', prob=0.5),
-    dict(
-        type='mmdet.PackDetInputs',
-        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'flip',
-                   'flip_direction'))
-]
+# train_pipeline1 = [
+#     *pre_transform,
+#     dict(
+#         type='Mosaic',
+#         img_scale=img_scale,
+#         pad_val=114.0,
+#         pre_transform=pre_transform),
+#     dict(
+#         type='YOLOv5RandomAffine',
+#         max_rotate_degree=0.0,
+#         max_shear_degree=0.0,
+#         scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
+#         # img_scale is (width, height)
+#         border=(-img_scale[0] // 2, -img_scale[1] // 2),
+#         border_val=(114, 114, 114)),
+#     dict(
+#         type='mmdet.Albu',
+#         transforms=albu_train_transforms,
+#         bbox_params=dict(
+#             type='BboxParams',
+#             format='pascal_voc',
+#             label_fields=['gt_bboxes_labels', 'gt_ignore_flags']),
+#         keymap={
+#             'img': 'image',
+#             'gt_bboxes': 'bboxes'
+#         }),
+#     dict(type='YOLOv5HSVRandomAug'),
+#     dict(type='mmdet.RandomFlip', prob=0.5),
+#     dict(
+#         type='mmdet.PackDetInputs',
+#         meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'flip',
+#                    'flip_direction'))
+# ]
 
 
-# train_dataloader1 = train_dataloader
-train_dataloader1 = dict(
-    batch_size=train_batch_size_per_gpu,
-    num_workers=train_num_workers,
-    persistent_workers=persistent_workers,
-    pin_memory=True,
-    sampler=dict(type='DefaultSampler', shuffle=True),
-    dataset=dict(
-        type=dataset_type,
-        data_root=data_root,
-        metainfo=metainfo,
-        ann_file='annotations/trainval.json',
-        data_prefix=dict(img='images/'),
-        filter_cfg=dict(filter_empty_gt=False, min_size=32),
-        pipeline=train_pipeline1),
-    collate_fn=dict(type='yolov5_collate'))
+# # train_dataloader1 = train_dataloader
+# train_dataloader1 = dict(
+#     batch_size=train_batch_size_per_gpu,
+#     num_workers=train_num_workers,
+#     persistent_workers=persistent_workers,
+#     pin_memory=True,
+#     sampler=dict(type='DefaultSampler', shuffle=True),
+#     dataset=dict(
+#         type=dataset_type,
+#         data_root=data_root,
+#         metainfo=metainfo,
+#         ann_file='annotations/trainval.json',
+#         data_prefix=dict(img='images/'),
+#         filter_cfg=dict(filter_empty_gt=False, min_size=32),
+#         pipeline=train_pipeline1),
+#     collate_fn=dict(type='yolov5_collate'))
 
 
 val_dataloader = dict(
@@ -137,6 +137,6 @@ default_hooks = dict(
 
 
 train_cfg = dict(
-    type='EpochBasedTrainLoopWith2Loaders',
+    # type='EpochBasedTrainLoopWith2Loaders',
     max_epochs=max_epochs,
     val_interval=save_checkpoint_intervals)
